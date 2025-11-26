6624e60c82c341a53a3e19839fd1617bbacc5185
Davide Gennnaioli <gennaioli.davide@franchettisalviani.net>
2025-11-26 10:34:49 +0100

    getCerchio
    get and set

--- GennaioliDavidePuntoCerchio/src/gennaiolidavidepuntocerchio/GennaioliDavidePuntoCerchio.java
+++ GennaioliDavidePuntoCerchio/src/gennaiolidavidepuntocerchio/GennaioliDavidePuntoCerchio.java
@@ -2,20 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
+package gennaiolidavidepuntocerchio;
 
 /**
  *
  * @author gennaioli.davide
  */
-public class GennaioliDavideConcessionario {
+public class GennaioliDavidePuntoCerchio {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        TestAuto a=new TestAuto();
-        a.avvia();
+        // TODO code application logic here
     }
     
 }
